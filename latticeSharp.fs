b05d67fccc1560e26dd6e820b931db14f93a9173
SaltyCatAgenda <ny.cassar@gmail.com>
2024-04-02 09:36:05 -0600

    Update latticeSharp.fs

--- latticeSharp.fs
+++ latticeSharp.fs
@@ -5,52 +5,27 @@ open MathNet.Numerics
 open Plotly.NET
 
 let rng = Random.shared
-let buildLattice n = Array2D.init n n (fun _ _ ->  2 * rng.Next(2) - 1)
+let buildLattice n = Array2D.init n n (fun _ _ ->  float (2 * rng.Next(2) - 1))
 
-let magnetization lattice = 
+let magnetization (lattice: float array2d) :float = 
     let length = 
-        lattice |> Array2D.length1
+         (lattice |> Array2D.length1)
     let r = 
-        Array.init length (fun i -> lattice.[i,*] |> Array.sum)
-    let mag:int = r |> Array.sum
-    mag
+        Seq.init length (fun i -> lattice.[i,*] |> Array.sum)
+    abs(float (r |> Seq.sum) / (float length**2))
 
-let Energy lattice : int= 
-    let mutable energy = 0
-    let eVec vec = vec |> Array.pairwise|>Array.map (fun (a,b) -> a*b)|> Array.sum
-    for n = 0 to (lattice |> Array2D.length1)-1 do
-        energy <- energy + (eVec lattice.[n,*]) + (eVec lattice.[*,n])
-    (-1) * energy
-
-/// Slower with these functions
-(*let neighbors lattice location = 
-    let a,b = location
+let Energy lattice : float= 
+    let mutable energy  = 0.0
     let length = lattice |> Array2D.length1
-    let edge = length - 1
-    let horizontal =
-        match b with
-        | b when b=0 -> lattice.[a,b+1]
-        | b when b=edge -> lattice.[a,b-1]
-        | b when b<edge && b>0 -> (lattice.[a,b-1] + lattice.[a,b+1])
-        |_ -> 0
-    let vertical =
-        match a with
-        | a when a=0 -> lattice.[a+1,b]
-        | a when a=edge -> lattice.[a-1,b]
-        | a when a>0 && a<edge -> lattice.[a-1,b] + lattice.[a+1,b]
-        |_ -> 0
-    horizontal + vertical
-let flipDE lattice location :int =
-    let a,b = location
-    let pairs = neighbors lattice location
-    let initE = lattice.[a,b] * pairs
-    let flipE = -1 * initE
-    (-1) * (flipE - initE)*)
-///
-let pointEnergy lattice location :int =
+    let eVec vec = vec |> Seq.pairwise|>Seq.map (fun (a,b) -> a*b)|> Seq.sum
+    for n = 0 to length-1 do
+        energy <- energy + (eVec lattice.[n,*]) + (eVec lattice.[*,n])
+    (-1.0) * energy
+
+let pointEnergy lattice location :float =
     let (a,b) = location
     let length = (lattice|>Array2D.length1) - 1
-    let pairs loc =
+    let pairs loc :float =
         match loc with
         | a,b when a=0 && b=0 -> lattice.[a,b] * (lattice.[1,0] + lattice.[0,1])
         | a,b when a=0 && b=length -> lattice.[a,b] * (lattice.[0,b-1] + lattice.[1,b])
@@ -64,46 +39,75 @@ let pointEnergy lattice location :int =
             lattice.[a,b] * (lattice.[a-1,b] + lattice.[a+1,b] + lattice.[a,b-1] + lattice.[a,b+1])
         | _,_ -> 0
     let initial = pairs location
-    let flip = -1 * initial
-    (-1) * (flip - initial)
+    let flip = -1.0 * initial
+    (-1.0) * (flip - initial)
     //2 * initial
 
-let mcmc lat (beta:float) (nflips:int) (tsteps:int) =
-    let lattice = lat |> Array2D.copy
+let mcmc (lat: float array2d) (beta:float) (nflips:int) (tsteps:int) : float array * float array =
+    let lattice: float array2d = lat |> Array2D.copy
     let length = lattice |> Array2D.length1
-    let mutable energy:int = Energy lattice
+    let mutable energy:float = Energy lattice
     let av_energy = Array.zeroCreate tsteps
-    let av_mag = Array.zeroCreate tsteps
+    let av_mag:float array = Array.zeroCreate tsteps
     for step=0 to tsteps-1 do
         for flip=0 to nflips-1 do
             let flip_i,flip_j = (rng.Next(length), rng.Next(length))
             let flip_dE = pointEnergy lattice (flip_i,flip_j)
             if exp (-beta * (float flip_dE) ) > rng.NextDouble() then
                 energy <- energy + flip_dE
-                lattice.[flip_i,flip_j] <- -1*lattice.[flip_i,flip_j]
+                lattice.[flip_i,flip_j] <- -1.0*lattice.[flip_i,flip_j]
         
-        av_energy[step] <- energy
-        av_mag[step] <- lattice |> magnetization
+        av_energy[step] <- energy/(float length**2)
+        av_mag[step] <- (lattice |> magnetization)
     (av_energy,av_mag)
 
 let lat20 = buildLattice 20
 
 let timer = new System.Diagnostics.Stopwatch()
 timer.Start()
-let e1,m1 = mcmc lat20 0.9 100000 20
+//let e1,m1 = mcmc lat20 0.6 100000 20
+let e1,m1 = mcmc (30|>buildLattice) 0.6 100000 20
 printfn "MCMC took %i milliseconds" timer.ElapsedMilliseconds
 
 let mc_tlist = [1..1..20]
 
 let ePlot = 
     Chart.Point(x=mc_tlist , y=e1)
-    |> Chart.withYAxisStyle (TitleText = "Energy ", MinMax=(-820, 0))
+    |> Chart.withYAxisStyle (TitleText = "Energy ", MinMax=(-2.2, 0))
     |> Chart.withXAxisStyle (TitleText = "Sample step")
     |> Chart.show
-    
+
+let f x = x + 5
+
 
 let mPlot = 
     Chart.Point(x=mc_tlist , y=m1)
-    |> Chart.withYAxisStyle (TitleText = "Magnetization ")
+    |> Chart.withYAxisStyle (TitleText = "Magnetization ", MinMax=(-1.1,1))
     |> Chart.withXAxisStyle (TitleText = "Sample step")
     |> Chart.show
+
+let EvsT, MvsT = 
+    let lattice = 30|> buildLattice
+    let bList = [0.42..0.001..0.47]
+    let length = bList |> List.length
+    let eList = Array.zeroCreate length
+    let mList = Array.zeroCreate length
+    for i=0 to (length-1) do        
+        let e1,m1 = (mcmc(lattice) bList.[i] 1000000 20) 
+        eList.[i] <- e1|>Array.last
+        mList.[i] <- m1|>Array.last   
+    eList,mList     
+
+let etPlot = 
+    Chart.Point(x=[0.35..0.01..0.55],y=EvsT)
+    |> Chart.withYAxisStyle (TitleText = "Energy ", MinMax=(-2.2, 0))
+    |> Chart.withXAxisStyle (TitleText = "Beta")
+    |> Chart.show
+let mtPlot = 
+    Chart.Point(x=[0.42..0.001..0.47],y=MvsT)
+    |> Chart.withYAxisStyle (TitleText = "Magnetization ", MinMax=(-0.1,1.05))
+    |> Chart.withXAxisStyle (TitleText = "Beta")
+    |> Chart.show
+
+
+let betaCrit = log(1.0 + sqrt(2.0))/2.0
