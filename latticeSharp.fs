5fe8f3facb56755fc24abdb3de8b26ff81c3fcc1
SaltyCatAgenda <ny.cassar@gmail.com>
2024-04-07 20:20:18 -0600

    Update latticeSharp.fs

--- latticeSharp.fs
+++ latticeSharp.fs
@@ -77,37 +77,40 @@ let ePlot =
     |> Chart.withXAxisStyle (TitleText = "Sample step")
     |> Chart.show
 
-let f x = x + 5
-
-
 let mPlot = 
     Chart.Point(x=mc_tlist , y=m1)
     |> Chart.withYAxisStyle (TitleText = "Magnetization ", MinMax=(-1.1,1))
     |> Chart.withXAxisStyle (TitleText = "Sample step")
     |> Chart.show
 
-let EvsT, MvsT = 
+let EvsT, MvsT, ChivsT = 
     let lattice = 30|> buildLattice
-    let bList = [0.42..0.001..0.47]
+    let bList = [0.2..0.01..0.6]
     let length = bList |> List.length
     let eList = Array.zeroCreate length
     let mList = Array.zeroCreate length
+    let chiList = Array.zeroCreate length
     for i=0 to (length-1) do        
-        let e1,m1 = (mcmc(lattice) bList.[i] 1000000 20) 
+        let e1,m1 = (mcmc(lattice) bList.[i] 100000 2000)
         eList.[i] <- e1|>Array.last
         mList.[i] <- m1|>Array.last   
-    eList,mList     
+        chiList.[i] <- abs((mList.[i]) - (m1 |> Array.average) )
+    eList,mList,chiList    
 
 let etPlot = 
-    Chart.Point(x=[0.35..0.01..0.55],y=EvsT)
+    Chart.Point(x=[0.2..0.01..0.6],y=EvsT)
     |> Chart.withYAxisStyle (TitleText = "Energy ", MinMax=(-2.2, 0))
     |> Chart.withXAxisStyle (TitleText = "Beta")
     |> Chart.show
 let mtPlot = 
-    Chart.Point(x=[0.42..0.001..0.47],y=MvsT)
+    Chart.Point(x=[0.2..0.01..0.6],y=MvsT)
     |> Chart.withYAxisStyle (TitleText = "Magnetization ", MinMax=(-0.1,1.05))
     |> Chart.withXAxisStyle (TitleText = "Beta")
     |> Chart.show
-
+let chiTplot = 
+    Chart.Point(x=[0.2..0.01..0.6], y=ChivsT)
+    |> Chart.withYAxisStyle (TitleText = "Chi ")
+    |> Chart.withXAxisStyle (TitleText = "Beta")
+    |> Chart.show
 
 let betaCrit = log(1.0 + sqrt(2.0))/2.0
